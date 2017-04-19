879d4dc91ef950d0e2beeb688d692754ee92e5cf
ThisIsRobokitty <ThisIsRobokitty@users.noreply.github.com>
2017-04-19 10:28:48 -0700

    almost done with gui
    added basic point generator

--- src/main.py
+++ src/main.py
@@ -1,25 +1,57 @@
-import map.points as point   
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog, QLabel, QFileDialog
+#import PyQt5.QtMultimedia as M 
+import gui.pwindow as pwin
 
+#import map.detector as dt
+import input.proj3keyboardinput as keyb
+#import render.particles.py as ren
+#import points.pointMaker as pm
+import map.points as point
 
-
-
-
-
-
-
-
-
-
-
+class TheGui(Q
+def __init__(self, parent = None):
+    QMainWindow.__init__(self, parent)
+    self.ui = pwin.Ui_MainWindow()
+    self.ui.setupUi(self)
 
 # main method
-def main():
+def main(self):
+    app = QApplication(sys.argv)
+    mainWindow = __init__(self)
+    mainWindow.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main(self)
+
+
+#gui methods
+def live(self, MainWindow):
     point.exitHamlet()
 
+def file(self, MainWindow):
+    keyb.main()
+    
+def keyboard(self, MainWindow):
+    deploy()
 
-##########################
-# main
-if __name__ == "__main__":
-    main()
 
-##########################
+
+
+
+
+def deploy():
+    exitflag = False
+    while not exitflag:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exitflag = True
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    exitflag = True
+
+        #main loop code
+        keyb.main()
+        point.exitHamlet()
+        #pon.draw()
