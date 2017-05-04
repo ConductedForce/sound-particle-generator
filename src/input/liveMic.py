773cad10dcdb4c85a3c95137990af18e74b73b3e
jantongiovanni <janton153@gmail.com>
2017-05-04 10:45:00 -0700

    update

--- src/input/liveMic.py
+++ src/input/liveMic.py
@@ -1,16 +1,26 @@
 import pyaudio
 import numpy
-import struct
 
-CHUNK = 128
+class mic():
 
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=CHUNK)
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100  
+    p = pyaudio.PyAudio()
 
-data = stream.read(CHUNK)
-print (numpy.fromstring(data, numpy.int16)) # use external numpy module
-print (struct.unpack('h'*CHUNK, data)) # use built-in struct module
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    def getChunkData():
+          data = stream.read(CHUNK)
+          return data
+       
+
+    def killMic():
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
