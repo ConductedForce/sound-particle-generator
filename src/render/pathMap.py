a33c14933dfe6a2430923cb94f6bd7a8adbef70a
Abel101 <abgonzalez@csumb.edu>
2017-05-11 18:42:09 -0700

    update 2

--- src/render/pathMap.py
+++ src/render/pathMap.py
@@ -1,5 +1,6 @@
 import random
 import math
+import pygame
 
 class Line():
     lsize = 0
@@ -23,7 +24,8 @@ class Point():
 
 
 class Path():
-   
+    startangle = []
+    angleList = [0,30,45,60,90,120,135,150,180,210,225,240,270,300,315,330,360]
     def findpath(self,mpoint):
         path1 = []
         path2 = []
@@ -48,7 +50,13 @@ class Path():
         #append p1 and p2   
         for i in mpoint:
             self.r = 50
-            angle = 2 * math.pi * random.random()
+            if(len(self.startangle) == 0):
+                angle = math.radians(self.angleList[random.randint(0,16)])
+                self.startangle.append(angle)   
+            else:
+                s = self.startangle [-1] + math.pi
+                angle = s + math.radians(self.angleList[random.randint(0,7)])
+
             point1 = Point(int(self.r*math.cos(angle))+i.x, int(self.r*math.sin(angle))+i.y)
             point2 = Point(int(self.r*math.cos(angle+math.pi))+i.x, int(self.r*math.sin(angle+math.pi))+i.y)
             self.listPoints1.append(point1)
@@ -90,10 +98,17 @@ class Path():
         b = point1.y - (m*point1.x)
         # yw - y1 / point2.x - point1.x for m
         # for loop
-        for i in range(point1.x,point2.x):
-            y = m*(i) + b
-            newPoint = Point( int(i), int(y) )
-            line.append(newPoint)
+        if ( point1.x > point2.x ):
+            for i in range(point2.x, point1.x):
+                y = (m*i) + b
+                newPoint = Point( int(i), int(y) )
+                line.append(newPoint)
+        else:
+            for i in range(point1.x,point2.x):
+                y = (m*i) + b
+                newPoint = Point( int(i), int(y) )
+                line.append(newPoint)
+
         newLine = line
         line = None
         return newLine
@@ -113,20 +128,46 @@ class Path():
                 daCount = count
         return (btw, mpoint[daCount])
 
+    def circlecontact(self,mpoint,point1,point2):
+        pygame.init()
+        for count,m in enumerate(mpoint):                  
+        # Centre of circle
+            Q = pygame.math.Vector2(m.x, m.y) 
+            r = 50               
+        # Radius of circle
+            P1 = pygame.math.Vector2(point1.x,point1.y)      
+        # Start of line segment
+            V = pygame.math.Vector2(point2.x,point2.y)-P1
+        # Vector along line segment
+            a = V.dot(V)
+            b = 2 * V.dot(P1 - Q)
+            c = P1.dot(P1) + Q.dot(Q) - 2 * P1.dot(Q) - r**2
+            disc = b**2 - 4 * a * c
+            if disc < 0:
+                return (False, Point(0,0))
+            sqrt_disc = math.sqrt(disc)
+            t1 = (-b + sqrt_disc) / (2 * a)
+            t2 = (-b - sqrt_disc) / (2 * a)
+            if not (0 <= t1 <= 1 or 0 <= t2 <= 1):
+                return (False,Point(0,0))
+            t = max(0, min(1, - b / (2 * a)))
+            intersect = P1 + t*V - Q == r
+            return (intersect, mpoint[count])
+
     def createHalfCircle(self, m, point1, point2):
         #creates a half circle with angle and radius
         #x = rcos(angle)
         #y = rsin(angle)
         #radius = distance from point 1 to point 2 / 2
-        r = self.distance(point1, point2)/2
+        r = self.r #.distance(point1, point2)/2
         #start angle = sin^-1 ( point1.y / r )
         #end angle = sin^-1 ( point2.y / r )
-        if (point1.y > m.y):
-            start = math.acos((m.x-point1.x) / self.r)
-            end = start + math.pi
-        else:
-            start = math.acos((m.x-point2.x) / self.r)
-            end = start + math.pi
+        #if (point1.y > m.y):
+        start = math.acos((m.x-point1.x) / self.r)
+        end = start + math.pi
+        #else:
+            #start = math.acos((m.x-point2.x) / self.r)
+            #end = start + math.pi
         #then cycle through angle degrees until you have the points of the circle
         circle = SemiCircle()
         #calculate mpoint
@@ -135,9 +176,9 @@ class Path():
         mh = m.x
         mk = m.y
 
-        start = math.floor(math.degrees(start))
-        end = math.floor(math.degrees(end))
-        for i in range(start, end):
+        nstart = math.floor(math.degrees(start))
+        nend = math.floor(math.degrees(end))
+        for i in range(nstart, nend):
             point = Point(int(r*math.cos(math.radians(i)))+mh, int(r*math.sin(math.radians(i)))+mk)
             circle.append(point)
         newCircle = circle
@@ -150,14 +191,16 @@ class Path():
         for count,i in enumerate(self.listPoints1):
             halfcreate=False
             if count+1 != len(self.listPoints1):
-                isBetween, m = self.between(mpoint,i,self.listPoints1[count+1])
+                #isBetween, m = self.between(mpoint,i,self.listPoints1[count+1])
+                isBetween, m = self.circlecontact(mpoint,i,self.listPoints1[count+1])
                 if isBetween:
                     path1.append(self.createHalfCircle(m, i, self.listPoints1[count+1]))
                     halfcreate=True
                 else:
                     path1.append(self.createLine(i,self.listPoints1[count+1]))
             else:
-                isBetween, m = self.between(mpoint,i,self.listPoints1[0])
+                #isBetween, m = self.between(mpoint,i,self.listPoints1[0])
+                isBetween, m = self.circlecontact(mpoint,i,self.listPoints1[0])
                 if isBetween:
                     path1.append(self.createHalfCircle(m, i, self.listPoints1[0]))
                     halfcreate=True
